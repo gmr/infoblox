b83ee11d3a9d522e6ab35cc8ffa53d7796dfb67b
Gavin M. Roy <gavinmroy@gmail.com>
2013-11-25 22:31:02 -0500

    Update session to support API needs of infoblox.record

--- infoblox/session.py
+++ infoblox/session.py
@@ -5,6 +5,8 @@ with the Infoblox NIOS device.
 import json
 import logging
 import requests
+import urllib
+import urlparse
 
 LOGGER = logging.getLogger(__name__)
 
@@ -14,9 +16,10 @@ PASSWORD = 'infoblox'
 
 class Session(object):
     """Central object for managing HTTP requests to the Infoblox appliance."""
+    BASE_PATH = '/wapi/v1.2'
     HEADERS = {'Content-type': 'application/json'}
 
-    def __init__(self, host, username=None, password=None):
+    def __init__(self, host, username=None, password=None, https=True):
         """Create a new instance of the Infoblox Session object
 
         :param str host: The Infoblox host to communicate with
@@ -25,31 +28,41 @@ class Session(object):
 
         """
         self.auth = (username or USERNAME, password or PASSWORD)
-        self.url = 'https://%s/wapi/v1.0/' % host
+        self.host = host
+        self.scheme = 'https' if https else 'http'
         self.session = requests.session()
 
-    def delete(self, ref):
+    def _request_url(self, path, query=None):
+        return urlparse.urlunparse((self.scheme,
+                                    self.host,
+                                    '/'.join([self.BASE_PATH, path]),
+                                    None,
+                                    urllib.urlencode(query) if query else None,
+                                    None))
+
+    def delete(self, path):
         """Call the Infoblox device to delete the ref
 
         :param str ref: The reference id
         :rtype: requests.Response
 
         """
-        return self.session.delete(self.url + ref,
+        return self.session.delete(self._request_url(path),
                                    auth=self.auth, verify=False)
 
-    def get(self, obj, data=None):
+    def get(self, path, data=None, return_fields=None):
         """Call the Infoblox device to get the obj for the data passed in
 
-        :param str obj: The object type
+        :param str obj_reference: The object reference data
         :param dict data: The data for the get request
         :rtype: requests.Response
 
         """
-        return self.session.get(self.url + obj, data=json.dumps(data or {}),
+        return self.session.get(self._request_url(path, return_fields),
+                                data=json.dumps(data),
                                 auth=self.auth, verify=False)
 
-    def post(self, obj, data):
+    def post(self, path, data):
         """Call the Infoblox device to post the obj for the data passed in
 
         :param str obj: The object type
@@ -57,6 +70,22 @@ class Session(object):
         :rtype: requests.Response
 
         """
-        return self.session.post(self.url + obj, data=json.dumps(data),
+        LOGGER.debug('Posting data: %r', data)
+        return self.session.post(self._request_url(path),
+                                 data=json.dumps(data or {}),
                                  headers=self.HEADERS, auth=self.auth,
                                  verify=False)
+
+    def put(self, path, data):
+        """Call the Infoblox device to post the obj for the data passed in
+
+        :param str obj: The object type
+        :param dict data: The data for the post
+        :rtype: requests.Response
+
+        """
+        LOGGER.debug('Putting data: %r', data)
+        return self.session.put(self._request_url(path),
+                                data=json.dumps(data or {}),
+                                headers=self.HEADERS, auth=self.auth,
+                                verify=False)
