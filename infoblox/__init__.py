aecb48d0ed59057aaa94e07e90f5ff23e0694e2a
Gavin M. Roy <gavinmroy@gmail.com>
2013-11-25 22:31:21 -0500

    Add shortcuts

--- infoblox/__init__.py
+++ infoblox/__init__.py
@@ -1,1 +1,8 @@
 __author__ = 'gmr'
+
+from infoblox.session import Session
+
+from infoblox.record import Host
+from infoblox.record import HostIPv4
+from infoblox.record import HostIPv6
+from infoblox.record import IPv4Address
